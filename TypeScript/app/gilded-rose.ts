0fb257099eac490428386dd89a35ff34fa874f91
Ryan Wessel <rwessel@dataminr.com>
2021-09-24 15:37:36 -0400

    Convert for loop to .map function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,10 +1,10 @@
 export enum Inventory {
-    AgedBrie = 'Aged Brie',
-    BackstagePass = 'Backstage passes to a TAFKAL80ETC concert',
-    Sulfuras = 'Sulfuras, Hand of Ragnaros',
-    DexterityVest = '+5 Dexterity Vest',
-    MongooseElixir = 'Elixir of the Mongoose',
-};
+    AgedBrie = "Aged Brie",
+    BackstagePass = "Backstage passes to a TAFKAL80ETC concert",
+    Sulfuras = "Sulfuras, Hand of Ragnaros",
+    DexterityVest = "+5 Dexterity Vest",
+    MongooseElixir = "Elixir of the Mongoose",
+}
 
 export class Item {
     name: string;
@@ -19,6 +19,7 @@ export class Item {
 }
 
 export class GildedRose {
+    MAX_QUALITY = 50;
     items: Array<Item>;
 
     constructor(items = [] as Array<Item>) {
@@ -26,52 +27,54 @@ export class GildedRose {
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != Inventory.AgedBrie && this.items[i].name != Inventory.BackstagePass) {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != Inventory.Sulfuras) {
-                        this.items[i].quality = this.items[i].quality - 1
+        return this.items.map((item) => {
+            if (
+                item.name != Inventory.AgedBrie &&
+                item.name != Inventory.BackstagePass
+            ) {
+                if (item.quality > 0) {
+                    if (item.name != Inventory.Sulfuras) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == Inventory.BackstagePass) {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.name == Inventory.BackstagePass) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
-            if (this.items[i].name != Inventory.Sulfuras) {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (item.name != Inventory.Sulfuras) {
+                item.sellIn = item.sellIn - 1;
             }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != Inventory.BackstagePass) {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != Inventory.Sulfuras) {
-                                this.items[i].quality = this.items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != Inventory.BackstagePass) {
+                        if (item.quality > 0) {
+                            if (item.name != Inventory.Sulfuras) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
-        }
-
-        return this.items;
+            return item;
+        });
     }
 }
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -2,153 +2,153 @@ import { expect } from "chai";
 import { Item, GildedRose, Inventory } from "../app/gilded-rose";
 
 const simulateDays = (gildedRose: GildedRose, days: number) => {
-  let items: Item[] = [];
-  for (let i = 0; i < days; i += 1) {
-    items = gildedRose.updateQuality();
-  }
-  return { items };
+    let items: Item[] = [];
+    for (let i = 0; i < days; i += 1) {
+        items = gildedRose.updateQuality();
+    }
+    return { items };
 };
 
 const qualityTest = ({
-  item,
-  sellIn,
-  quality,
-  duration,
-  expected,
+    item,
+    sellIn,
+    quality,
+    duration,
+    expected,
 }: {
-  item: Inventory;
-  sellIn: number;
-  quality: number;
-  duration: number;
-  expected: number;
+    item: Inventory;
+    sellIn: number;
+    quality: number;
+    duration: number;
+    expected: number;
 }) => {
-  const gildedRose = new GildedRose([new Item(item, sellIn, quality)]);
-  const { items } = simulateDays(gildedRose, duration);
-  expect(items[0].quality).to.equal(expected);
+    const gildedRose = new GildedRose([new Item(item, sellIn, quality)]);
+    const { items } = simulateDays(gildedRose, duration);
+    expect(items[0].quality).to.equal(expected);
 };
 
 describe("Gilded Rose", () => {
-  describe("General Items", () => {
-    it("the quality of items should degrade by 1 at the end of each day", () =>
-      qualityTest({
-        item: Inventory.MongooseElixir,
-        sellIn: 20,
-        quality: 20,
-        duration: 20,
-        expected: 0,
-      }));
+    describe("General Items", () => {
+        it("the quality of items should degrade by 1 at the end of each day", () =>
+            qualityTest({
+                item: Inventory.MongooseElixir,
+                sellIn: 20,
+                quality: 20,
+                duration: 20,
+                expected: 0,
+            }));
 
-    it("the quality of items should degrade by 2 at the end of each day if their sell by date has passed", () =>
-      qualityTest({
-        item: Inventory.MongooseElixir,
-        sellIn: 0,
-        quality: 20,
-        duration: 10,
-        expected: 0,
-      }));
+        it("the quality of items should degrade by 2 at the end of each day if their sell by date has passed", () =>
+            qualityTest({
+                item: Inventory.MongooseElixir,
+                sellIn: 0,
+                quality: 20,
+                duration: 10,
+                expected: 0,
+            }));
 
-    it("the quality of an item should never degrade below 0", () =>
-      qualityTest({
-        item: Inventory.MongooseElixir,
-        sellIn: 0,
-        quality: 20,
-        duration: 100,
-        expected: 0,
-      }));
-  });
-
-  describe(Inventory.Sulfuras, () => {
-    it("quality should never degrade", () =>
-      qualityTest({
-        item: Inventory.Sulfuras,
-        sellIn: 0,
-        quality: 80,
-        duration: 100,
-        expected: 80,
-      }));
-
-    it("sellIn value should never decrease", () => {
-      const gildedRose = new GildedRose([
-        new Item(Inventory.Sulfuras, 100, 80),
-      ]);
-      const { items } = simulateDays(gildedRose, 100);
-      expect(items[0].sellIn).to.equal(100);
+        it("the quality of an item should never degrade below 0", () =>
+            qualityTest({
+                item: Inventory.MongooseElixir,
+                sellIn: 0,
+                quality: 20,
+                duration: 100,
+                expected: 0,
+            }));
     });
-  });
 
-  describe(Inventory.AgedBrie, () => {
-    it("quality should increase in quality by 1 at the end of each day", () =>
-      qualityTest({
-        item: Inventory.AgedBrie,
-        sellIn: 20,
-        quality: 0,
-        duration: 20,
-        expected: 20,
-      }));
+    describe(Inventory.Sulfuras, () => {
+        it("quality should never degrade", () =>
+            qualityTest({
+                item: Inventory.Sulfuras,
+                sellIn: 0,
+                quality: 80,
+                duration: 100,
+                expected: 80,
+            }));
 
-    it("quality should increase in quality by 2 at the end of each day after the sell by date has passed", () =>
-      qualityTest({
-        item: Inventory.AgedBrie,
-        sellIn: 0,
-        quality: 0,
-        duration: 20,
-        expected: 40,
-      }));
+        it("sellIn value should never decrease", () => {
+            const gildedRose = new GildedRose([
+                new Item(Inventory.Sulfuras, 100, 80),
+            ]);
+            const { items } = simulateDays(gildedRose, 100);
+            expect(items[0].sellIn).to.equal(100);
+        });
+    });
 
-    it("quality should not increase beyond 50", () =>
-      qualityTest({
-        item: Inventory.AgedBrie,
-        sellIn: 0,
-        quality: 0,
-        duration: 100,
-        expected: 50,
-      }));
-  });
+    describe(Inventory.AgedBrie, () => {
+        it("quality should increase in quality by 1 at the end of each day", () =>
+            qualityTest({
+                item: Inventory.AgedBrie,
+                sellIn: 20,
+                quality: 0,
+                duration: 20,
+                expected: 20,
+            }));
 
-  describe(Inventory.BackstagePass, () => {
-    it("quality should increase by 1 at the end of each day when its sell by date is more than 10 days away", () => 
-        qualityTest({
-            item: Inventory.BackstagePass,
-            sellIn: 50,
-            quality: 0,
-            duration: 40,
-            expected: 40,
-        }));
+        it("quality should increase in quality by 2 at the end of each day after the sell by date has passed", () =>
+            qualityTest({
+                item: Inventory.AgedBrie,
+                sellIn: 0,
+                quality: 0,
+                duration: 20,
+                expected: 40,
+            }));
 
-    it("quality should increase by 2 at the end of each day when its sell by date is between 6-10 days away", () => 
-        qualityTest({
-            item: Inventory.BackstagePass,
-            sellIn: 10,
-            quality: 10,
-            duration: 5,
-            expected: 20,
-        }));
+        it("quality should not increase beyond 50", () =>
+            qualityTest({
+                item: Inventory.AgedBrie,
+                sellIn: 0,
+                quality: 0,
+                duration: 100,
+                expected: 50,
+            }));
+    });
 
-    it("quality should increase by 3 at the end of each day when its sell by date is between 0-5 days away", () => 
-        qualityTest({
-            item: Inventory.BackstagePass,
-            sellIn: 5,
-            quality: 10,
-            duration: 5,
-            expected: 25,
-        }));
+    describe(Inventory.BackstagePass, () => {
+        it("quality should increase by 1 at the end of each day when its sell by date is more than 10 days away", () =>
+            qualityTest({
+                item: Inventory.BackstagePass,
+                sellIn: 50,
+                quality: 0,
+                duration: 40,
+                expected: 40,
+            }));
 
-    it("quality should drop to 0 once the sell by date has passed", () => 
-        qualityTest({
-            item: Inventory.BackstagePass,
-            sellIn: 10,
-            quality: 0,
-            duration: 11,
-            expected: 0,
-        }));
+        it("quality should increase by 2 at the end of each day when its sell by date is between 6-10 days away", () =>
+            qualityTest({
+                item: Inventory.BackstagePass,
+                sellIn: 10,
+                quality: 10,
+                duration: 5,
+                expected: 20,
+            }));
 
-    it("quality should not increase beyond 50", () => 
-        qualityTest({
-            item: Inventory.BackstagePass,
-            sellIn: 50,
-            quality: 0,
-            duration: 50,
-            expected: 50,
-        }));
-  });
+        it("quality should increase by 3 at the end of each day when its sell by date is between 0-5 days away", () =>
+            qualityTest({
+                item: Inventory.BackstagePass,
+                sellIn: 5,
+                quality: 10,
+                duration: 5,
+                expected: 25,
+            }));
+
+        it("quality should drop to 0 once the sell by date has passed", () =>
+            qualityTest({
+                item: Inventory.BackstagePass,
+                sellIn: 10,
+                quality: 0,
+                duration: 11,
+                expected: 0,
+            }));
+
+        it("quality should not increase beyond 50", () =>
+            qualityTest({
+                item: Inventory.BackstagePass,
+                sellIn: 50,
+                quality: 0,
+                duration: 50,
+                expected: 50,
+            }));
+    });
 });
