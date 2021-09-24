f190fd3a65c92482e1f60c546218152286892e25
Ryan Wessel <rwessel@dataminr.com>
2021-09-24 17:50:15 -0400

    Create distinct pipelines for each product type

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -27,58 +27,68 @@ export class GildedRose {
         this.items = items;
     }
 
+    static isLegendaryItem(item) {
+        return item.name === Inventory.Sulfuras;
+    }
+
+    static isBrie(item) {
+        return item.name === Inventory.AgedBrie;
+    }
+
+    static isBackstagePass(item) {
+        return item.name === Inventory.BackstagePass;
+    }
+
+    static isConjuredItem(item) {
+        return item.name === Inventory.ConjuredManaCake;
+    }
+
+    static isGeneralItem(item) {
+        return (
+            !GildedRose.isBackstagePass(item) &&
+            !GildedRose.isBrie(item) &&
+            !GildedRose.isConjuredItem(item) &&
+            !GildedRose.isLegendaryItem(item)
+        );
+    }
+
     decrementSellIn(item) {
-        if (item.name != Inventory.Sulfuras) {
-            item.sellIn -= 1;
-        }
+        item.sellIn -= 1;
         return item;
     }
 
     updateGeneralItemQuality(item) {
-        if (
-            item.name !== Inventory.AgedBrie &&
-            item.name !== Inventory.BackstagePass &&
-            item.name !== Inventory.Sulfuras &&
-            item.name !== Inventory.ConjuredManaCake
-        ) {
-            if (item.sellIn < 0 && item.quality > 0) {
-                item.quality -= 1;
-            }
-            if (item.quality > 0) {
-                item.quality -= 1;
-            }
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality -= 1;
+        }
+        if (item.quality > 0) {
+            item.quality -= 1;
         }
         return item;
     }
 
     updateBrieQuality(item) {
-        if (item.name === Inventory.AgedBrie) {
-            if (item.sellIn < 0 && item.quality < 50) {
-                item.quality += 2;
-            } else if (item.quality < 50) {
-                item.quality += 1;
-            }
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality += 2;
+        } else if (item.quality < 50) {
+            item.quality += 1;
         }
         return item;
     }
 
     updateBackstagePassQuality(item) {
-        if (item.name === Inventory.BackstagePass) {
-            if (item.sellIn < 0) {
-                item.quality = 0;
-            } else if (item.quality < 50) {
-                item.quality += 1;
-                if (item.name == Inventory.BackstagePass) {
-                    if (item.sellIn < 10) {
-                        if (item.quality < 50) {
-                            item.quality += 1;
-                        }
-                    }
-                    if (item.sellIn < 5) {
-                        if (item.quality < 50) {
-                            item.quality += 1;
-                        }
-                    }
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.quality < 50) {
+            item.quality += 1;
+            if (item.sellIn < 10) {
+                if (item.quality < 50) {
+                    item.quality += 1;
+                }
+            }
+            if (item.sellIn < 5) {
+                if (item.quality < 50) {
+                    item.quality += 1;
                 }
             }
         }
@@ -86,23 +96,34 @@ export class GildedRose {
     }
 
     updateConjuredItemQuality(item) {
-        if (item.name === Inventory.ConjuredManaCake) {
-            if (item.sellIn < 0 && item.quality > 0) {
-                item.quality -= 2;
-            }
-            if (item.quality > 0) {
-                item.quality -= 2;
-            }
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality -= 2;
+        }
+        if (item.quality > 0) {
+            item.quality -= 2;
         }
         return item;
     }
 
     updateQuality() {
-        return this.items
-            .map(this.decrementSellIn)
-            .map(this.updateGeneralItemQuality)
-            .map(this.updateBrieQuality)
-            .map(this.updateBackstagePassQuality)
+        this.items
+            .filter((item) => !GildedRose.isLegendaryItem(item))
+            .map(this.decrementSellIn);
+
+        this.items
+            .filter(GildedRose.isGeneralItem)
+            .map(this.updateGeneralItemQuality);
+
+        this.items
+            .filter(GildedRose.isConjuredItem)
             .map(this.updateConjuredItemQuality);
+
+        this.items.filter(GildedRose.isBrie).map(this.updateBrieQuality);
+
+        this.items
+            .filter(GildedRose.isBackstagePass)
+            .map(this.updateBackstagePassQuality);
+
+        return this.items;
     }
 }
